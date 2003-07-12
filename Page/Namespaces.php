04528d9a0ac72b05fc87dc8e5849f6df2e5cd293
Klaus Guenther <thesaur@php.net>
2003-07-12 23:43:20 +0000

    change structure to match doctypes
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_Page/trunk@134926 c90b9560-bf6c-de11-be94-00142212c4b1

--- Page/Namespaces.php
+++ Page/Namespaces.php
@@ -26,9 +26,9 @@
  */
 
 // Array of namespace declarations:
-$namespace['xhtml']['1.0'] = 'http://www.w3.org/1999/xhtml';
-$namespace['xhtml']['basic']['1.0'] = 'http://www.w3.org/1999/xhtml';
-$namespace['xhtml']['1.1'] = 'http://www.w3.org/1999/xhtml';
-$namespace['xhtml']['2.0'] = 'http://www.w3.org/2002/06/xhtml2';
+$namespace['xhtml']['1.0'][] = 'http://www.w3.org/1999/xhtml';
+$namespace['xhtml']['basic']['1.0'][] = 'http://www.w3.org/1999/xhtml';
+$namespace['xhtml']['1.1'][] = 'http://www.w3.org/1999/xhtml';
+$namespace['xhtml']['2.0'][] = 'http://www.w3.org/2002/06/xhtml2';
 
 ?>
